306fbdea87e9e1d72f15cf4a076b9c205d998db4
Sandesh Kumar K S <sandeshkumar555dreams@gmail.com>
2021-05-08 01:26:30 +0530

    Updated

--- src/components/my-carousal/my-carousal.component.jsx
+++ src/components/my-carousal/my-carousal.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import Slide1 from "../../assets/img/carousal/slide1.webp";
-import Slide3 from "../../assets/img/carousal/slide3.webp";
+//import Slide1 from "../../assets/img/carousal/slide1.webp";
+//import Slide3 from "../../assets/img/carousal/slide3.webp";
 import ScrollDown from "../scroll-down/scroll-down.component";
 import "./my-carousal.styles.css";
 
